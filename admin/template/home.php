cf929a150dc07f87f438ffeda96422c60e99c62a
Owen <itsowenlu@gmail.com>
2024-04-27 00:03:59 +0700

    Update home.php
    fix dashboard view

--- admin/template/home.php
+++ admin/template/home.php
@@ -1,28 +1,25 @@
 <h3>Dashboard</h3>
 <br/>
 <?php 
-	$sql=" select * from barang where stok <= 3";
-	$row = $config -> prepare($sql);
-	$row -> execute();
-	$r = $row -> rowCount();
-	if($r > 0){
+    $sql=" select * from barang where stok <= 3";
+    $row = $config -> prepare($sql);
+    $row -> execute();
+    $r = $row -> rowCount();
+    if($r > 0){
 ?>
-
-<?php
-		echo "
-		<div class='alert alert-warning'>
-			<span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'>$r</span> barang yang Stok tersisa sudah kurang dari 3 items. silahkan pesan lagi !!
-			<span class='pull-right'><a href='index.php?page=barang&stok=yes'>Tabel Barang <i class='fa fa-angle-double-right'></i></a></span>
-		</div>
-		";	
-	}
+    <div class='alert alert-warning'>
+        <span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'><?php echo $r; ?></span> barang yang Stok tersisa sudah kurang dari 3 items. silahkan pesan lagi !!
+        <span class='pull-right'><a href='index.php?page=barang&stok=yes'>Tabel Barang <i class='fa fa-angle-double-right'></i></a></span>
+    </div>
+<?php   
+    }
 ?>
 <?php $hasil_barang = $lihat -> barang_row();?>
 <?php $hasil_kategori = $lihat -> kategori_row();?>
 <?php $stok = $lihat -> barang_stok_row();?>
 <?php $jual = $lihat -> jual_row();?>
 <div class="row">
-    <div class="col-md-3 mb-3" style="padding-right: 15px; padding-left: 15px;">
+    <div class="col-md-3 mb-3">
         <div class="card">
             <div class="card-header bg-primary text-white">
                 <h6 class="pt-2"><i class="fas fa-cubes"></i> Barang</h6>
@@ -35,20 +32,40 @@
             </div>
         </div>
     </div>
-    <div class="col-md-3 mb-3" style="padding-right: 15px; padding-left: 15px;">
+    <?php
+    $totalBeli = 0;
+    $totalJual = 0;
+    $totalStok = 0;
+    $restockCount = 0; // Counter for products needing restock
+
+    if ($_GET['stok'] == 'yes') {
+        $hasil = $lihat->barang_stok();
+    } else {
+        $hasil = $lihat->barang();
+    }
+
+    foreach ($hasil as $isi) {
+        if ($isi['stok'] <= 3) {
+            $restockCount++; // Increment restock counter
+        }
+    }
+    ?>
+
+    <div class="col-md-3 mb-3">
         <div class="card">
             <div class="card-header bg-primary text-white">
-                <h6 class="pt-2"><i class="fas fa-upload"></i> Telah Terjual</h6>
+                <h6 class="pt-2"><i class="fa fa-exclamation-triangle"></i> Barang Perlu Restok</h6>
             </div>
             <div class="card-body">
-                <center><h1><?php echo number_format($jual['stok']); ?></h1></center>
+                <center><h1><?php echo number_format($restockCount); ?></h1></center>
             </div>
             <div class="card-footer">
-                <a href='index.php?page=laporan'>List Penjualan <i class='fa fa-angle-double-right'></i></a>
+                <a href='index.php?page=barang&stok=yes'>Lihat Detil <i class='fa fa-angle-double-right'></i></a>
             </div>
         </div>
     </div>
-    <div class="col-md-3 mb-3" style="padding-right: 15px; padding-left: 15px;">
+
+    <div class="col-md-3 mb-3">
         <div class="card">
             <div class="card-header bg-primary text-white">
                 <h6 class="pt-2"><i class="fa fa-bookmark"></i> Kategori Barang</h6>
@@ -61,7 +78,7 @@
             </div>
         </div>
     </div>
-    <!-- Add another card if needed, ensuring it uses col-md-3 and the total does not exceed 12 columns in a row -->
+
     <?php 
     date_default_timezone_set('Your/Timezone'); // Set the appropriate timezone
 
@@ -107,28 +124,27 @@ $bulan_tes = array(
     $profit = $bayar - $modal;
 ?>
 
-<div class="col-md-3 mb-3" style="padding-right: 15px; padding-left: 15px;">
-    <div class="card">
-        <div class="card-header bg-primary text-white">
-            <h6 class="pt-2"><i class="fa fa-dollar-sign"></i> Profit Bulanan <?= $bulan_tes[$bulan_sekarang]; ?> <?= $tahun_sekarang; ?></h6>
-        </div>
-        <div class="card-body">
-            <center>
-                <?php
-$profit_formatted = number_format($profit); // Format profit for display
-$text_color = $profit >= 0 ? 'green' : 'red'; // Determine text color based on profit
 
-// Output the profit with appropriate color
-echo "<h3 style='color: $text_color;'>Rp.$profit_formatted,-</h3>";
-?>
+    <div class="col-md-3 mb-3">
+        <div class="card">
+            <div class="card-header bg-primary text-white">
+                <h6 class="pt-2"><i class="fa fa-dollar-sign"></i> Profit Bulanan <?= $bulan_tes[$bulan_sekarang]; ?> <?= $tahun_sekarang; ?></h6>
+            </div>
+            <div class="card-body">
+                <center>
+                    <?php
+                    $profit_formatted = number_format($profit); // Format profit for display
+                    $text_color = $profit >= 0 ? 'green' : 'red'; // Determine text color based on profit
 
-            </center>
-        </div>
-        <div class="card-footer">
-            <a href='index.php?page=laporan'>Lihat Laporan <i class='fa fa-angle-double-right'></i></a>
+                    // Output the profit with appropriate color
+                    echo "<h3 style='color: $text_color;'>Rp.$profit_formatted,-</h3>";
+                    ?>
+                </center>
+            </div>
+            <div class="card-footer">
+                <a href='index.php?page=laporan'>Lihat Laporan <i class='fa fa-angle-double-right'></i></a>
+            </div>
         </div>
     </div>
 </div>
 
-
-</div>
